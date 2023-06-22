fd6dd6641e46a5f24e8b08bfcb1853a45abff978
Joel Jones <chambakatajoel@gmail.com>
2023-06-22 17:15:00 +0200

    The people paid taxes accurately

--- tax_calculator.py
+++ tax_calculator.py
@@ -5,11 +5,11 @@ income = float(input("Enter the annual income: "))
 #
 tax = 0
 if income<=85_528:
-    tax = 0.18*income - 556
+    tax = 0.18*income - 556.02
 elif income>85_528:
-    tax = 0.32 * (income - 85_528) + 14_839
+    tax = 0.32 * (income - 85_528) + 14_839.02
     
 if tax < 0 :
-    tax = 0
+    tax = 0.0
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
