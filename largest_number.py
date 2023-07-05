00b2ac5c728efa3e592b7ac18f737acb4bdaedeb
Joel Jones <chambakatajoel@gmail.com>
2023-07-05 08:03:00 +0200

    Using a while loop

--- largest_number.py
+++ largest_number.py
@@ -8,16 +8,35 @@
 #     largest_number = number
 # # Go to line 02
 
-#if we are to use the max() and min() function
-# Read three numbers.
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-number3 = int(input("Enter the third number: "))
+# #if we are to use the max() and min() function
+# # Read three numbers.
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+# number3 = int(input("Enter the third number: "))
 
-# Check which one of the numbers is the greatest
-# and pass it to the largest_number variable.
+# # Check which one of the numbers is the greatest
+# # and pass it to the largest_number variable.
 
-largest_number = max(number1, number2, number3)
+# largest_number = max(number1, number2, number3)
 
-# Print the result.
+# # Print the result.
+# print("The largest number is:", largest_number)
+
+# Store the current largest number here.
+largest_number = -999999999
+
+# Input the first value.
+number = int(input("Enter a number or type -1 to stop: "))
+
+# If the number is not equal to -1, continue.
+while number != -1:
+    # Is number larger than largest_number?
+    if number > largest_number:
+        # Yes, update largest_number.
+        largest_number = number
+    # Input the next number.
+    number = int(input("Enter a number or type -1 to stop: "))
+
+# Print the largest number.
 print("The largest number is:", largest_number)
+
