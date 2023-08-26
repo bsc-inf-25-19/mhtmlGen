21fcb4444e47d8901b278feebc6b6ecc2af152c3
Joel Jones <chambakatajoel@gmail.com>
2023-08-26 20:15:00 +0200

    refined verbally defined rules

--- pyramid_bilders.py
+++ pyramid_bilders.py
@@ -3,11 +3,12 @@ blocks = int(input("Enter the number of blocks: "))
 #
 # Write your code here.
 #	
-
 height = 0
+layer_blocks = 1  # Start with the first layer requiring 1 block
 
-while blocks > 0:
-    blocks -= height + 1
-    if blocks>0:height += 1
+while blocks >= layer_blocks:
+    blocks -= layer_blocks
+    height += 1
+    layer_blocks += 1  # Each new layer needs one more block than the previous layer
 
-print("The height of the pyramid:", blocks, height)
+print("The height of the pyramid:", height)
